9a437a43c1fdc2202682a56173b18f5c1ccabe46
Sayantan Khan <bolt.khan@gmail.com>
2017-07-05 18:33:18 +0200

    technical results

--- Notes/Technical Results/auto/technical_results.el
+++ Notes/Technical Results/auto/technical_results.el
@@ -1,16 +1,16 @@
 (TeX-add-style-hook
- "homotopy_theory_notes"
+ "technical_results"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "12pt" "notitlepage")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("babel" "english") ("tocbibind" "nottoc") ("geometry" "left=3cm" "right=3cm" "top=2cm" "bottom=2cm")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -33,12 +33,15 @@
     "gentium")
    (TeX-add-symbols
     '("cat" 1)
-    "id"
-    "homtheorie"
-    "redhom")
+    "cohomtheorie"
+    "calz"
+    "colim")
    (LaTeX-add-labels
-    "sec:gener-cohom-theor"
-    "sec:corr-betw-homol")
+    "sec:comp-theor-cohom"
+    "sec:browns-repr-theor"
+    "sec:spectra-cohom-theor"
+    "sec:definitions-notation"
+    "sec:some-useful-lemmas")
    (LaTeX-add-amsthm-newtheorems
     "thm"
     "lem"
@@ -46,6 +49,7 @@
     "cor"
     "conj"
     "exmp"
+    "remark"
     "defn"))
  :latex)
 
