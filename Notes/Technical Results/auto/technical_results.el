01bbe8119eff4d910958f2ab3105ba5a0715e5ad
Sayantan Khan <bolt.khan@gmail.com>
2017-07-08 13:56:38 +0200

    started proof

--- Notes/Technical Results/auto/technical_results.el
+++ Notes/Technical Results/auto/technical_results.el
@@ -5,12 +5,12 @@
                      '(("article" "12pt" "notitlepage")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("babel" "english") ("tocbibind" "nottoc") ("geometry" "left=3cm" "right=3cm" "top=2cm" "bottom=2cm")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -37,11 +37,14 @@
     "calz"
     "colim")
    (LaTeX-add-labels
+    "sec:blak-mass-theor"
     "sec:comp-theor-cohom"
     "sec:browns-repr-theor"
     "sec:spectra-cohom-theor"
+    "sec:proof-browns-repr"
     "sec:definitions-notation"
-    "sec:some-useful-lemmas")
+    "sec:some-useful-lemmas"
+    "thm-milnor")
    (LaTeX-add-amsthm-newtheorems
     "thm"
     "lem"
