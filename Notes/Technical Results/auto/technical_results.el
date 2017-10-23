e88a35c3ef26cf450c762873522c14daf0db39de
Sayantan Khan <sayantangkhan@gmail.com>
2017-10-23 22:52:25 +0530

    refactor

--- Notes/Technical Results/auto/technical_results.el
+++ Notes/Technical Results/auto/technical_results.el
@@ -1,37 +1,13 @@
 (TeX-add-style-hook
  "technical_results"
  (lambda ()
-   (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("article" "12pt" "notitlepage")))
-   (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("babel" "english") ("tocbibind" "nottoc") ("geometry" "left=3cm" "right=3cm" "top=2cm" "bottom=2cm")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
-   (TeX-run-style-hooks
-    "latex2e"
-    "article"
-    "art12"
-    "inputenc"
-    "graphicx"
-    "babel"
-    "tocbibind"
-    "hyperref"
-    "geometry"
-    "bbm"
-    "amsmath"
-    "amsthm"
-    "amsfonts"
-    "amssymb"
-    "mathtools"
-    "tikz-cd"
-    "xcolor"
-    "gentium"
-    "todonotes")
    (TeX-add-symbols
     '("cat" 1)
     "cohomtheorie"
@@ -58,15 +34,6 @@
     "thm-lim1"
     "thm-milnor")
    (LaTeX-add-bibliographies
-    "references")
-   (LaTeX-add-amsthm-newtheorems
-    "thm"
-    "lem"
-    "prop"
-    "cor"
-    "conj"
-    "exmp"
-    "remark"
-    "defn"))
+    "references"))
  :latex)
 
